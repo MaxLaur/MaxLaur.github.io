b182e46c03af9f46b8e60eb88c5cdaf5db2949c3
Max Laurendeau <maxlaurend85@gmail.com>
2024-04-10 16:17:41 -0400

    visual effects section for toggle buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { useEffect, useState } from 'react';
 import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
 import { Toggle } from "@/components/ui/toggle"
-import { ReactTerminal } from "react-terminal";
 import Terminal from '@/components/Terminal';
 import lazer_grid from '../../public/lazer_grid.png'
 import "./globals.css";
@@ -14,11 +13,6 @@ export default function Home() {
   const [scanLinesToggle, setScanLinesToggle] = useState<boolean>(true);
   const [scanLineToggle, setScanLineToggle] = useState<boolean>(true);
   const [flickerToggle, setFlickerToggle] = useState<boolean>(true);
-
-  //React terminal commands
-  // const commands = {
-  //   whoami: "jackharper",
-  // };
 
   useEffect(() => {
     setLoaded(true);
@@ -49,21 +43,31 @@ export default function Home() {
         <a href="mailto:maxlaurend85@gmail.com">
           <Button variant="ghost" className='m-2 mr-10 hover:bg-purple-400 hover:text-green-300' >contact</Button>
         </a>
-        <Toggle
-          defaultPressed
-          className='m-1 p-1 text-purple-400'
-          onClick={()=>{setFlickerToggle(!flickerToggle)}}>
-            Flicker
-        </Toggle>
-        <Toggle
-          defaultPressed
-          className='m-1 p-1 text-purple-400'
-          onClick={()=>{
-            setScanLinesToggle(!scanLinesToggle)
-            setScanLineToggle(!scanLineToggle)
-          }}>
-          Scanline
-        </Toggle>
+        <div className='flex'>
+          <div className='flex flex-col mr-2 mt-2'>
+            <p>visual</p>
+            <p>effects</p>
+          </div>
+          
+          <div className='flex flex-col'>
+            <Toggle
+              defaultPressed
+              className='m-1 p-1 text-purple-400'
+              onClick={()=>{setFlickerToggle(!flickerToggle)}}>
+                Flicker
+            </Toggle>
+            <Toggle
+              defaultPressed
+              className='m-1 p-1 text-purple-400'
+              onClick={()=>{
+                setScanLinesToggle(!scanLinesToggle)
+                setScanLineToggle(!scanLineToggle)
+              }}>
+              Scanline
+            </Toggle>
+          </div>
+        </div>
+        
       </nav>
       <div 
         className="absolute inset-0 bg-black opacity-10 animate-pulse-opacity"
@@ -77,17 +81,11 @@ export default function Home() {
       <Card className='sm:w-5/6 md:w-5/6 lg:w-4/6 xl:w-3/6 opacity-85 mb-5 mt-5 pt-5'>
         <CardContent>
           <p className='text-green-300'>
-            Hi, I&apos;m Max and I am a web developer, welcome to my page! Here you will find a 
+            Hi, my name is Max and I am a web developer, welcome to my page! Here you will find a 
             bit of information about me and a collection of projects I have worked on. Use the terminal or the buttons above to navigate across my website.
           </p>
         </CardContent>
       </Card>
-
-      {/* <ReactTerminal
-        commands={commands}
-        theme="matrix"
-        showControlBar="false"  
-      /> */}
 
       <Terminal resumeDialog={resumeDialog} setResumeDialog={setResumeDialog}/>
     </main>
