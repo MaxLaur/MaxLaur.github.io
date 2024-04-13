263ca13d594b967dc963d476043f8f02b4b4b258
Max Laurendeau <maxlaurend85@gmail.com>
2024-04-12 21:31:41 -0400

    Responsive drop down nav menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,20 @@ import { useState } from 'react';
 import Link from 'next/link';
 import { Toggle } from "@/components/ui/toggle"
 import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 const Navbar = () => {
   const [scanLinesToggle, setScanLinesToggle] = useState<boolean>(true);
@@ -23,25 +37,59 @@ const Navbar = () => {
         flickerToggle &&
         <div className='flicker'></div>
       }
-      <nav className='flex justify-end w-full'>
-      <Link href="/">
-          <Button variant="ghost" className='m-2 mr-4 hover:bg-purple-400 hover:text-green-300 text-xl' >Home</Button>
-        </Link>
-        <Link href="https://drive.google.com/file/d/1lc-Lc300vg4dhbu7hITuV4P9kufxWEP9/view?usp=drive_link" target="blank">
-          <Button variant="ghost" className='m-2 hover:bg-purple-400 hover:text-green-300 text-xl'>Resume</Button>
-        </Link>
-        {/* <a href="mailto:maxlaurend85@gmail.com">
-          <Button variant="ghost" className='m-2 mr-10 hover:bg-purple-400 hover:text-green-300 text-xl' >contact</Button>
-        </a> */}
-        <Link href="/projects">
-          <Button variant="ghost" className='m-2 mr-10 hover:bg-purple-400 hover:text-green-300 text-xl' >Projects</Button>
-        </Link>
+      <nav className='flex md:justify-end justify-between w-full mb-4'>
+        {/* Hamburger menu for small breakpoints */}
+        <div className="block md:hidden ml-3 mt-3">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" className='text-green-300 text-xl bg-purple-500'>MENU</Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56">
+              <DropdownMenuGroup>
+                <Link href='/'>
+                  <DropdownMenuItem className='cursor-pointer text-purple-400'>Home</DropdownMenuItem>
+                </Link>
+                <Link href='/projects'>
+                  <DropdownMenuItem className='cursor-pointer text-purple-400'>Projects</DropdownMenuItem>
+                </Link>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+              <Link href='/projects'>
+                <DropdownMenuItem className='cursor-pointer text-purple-400'>Resume</DropdownMenuItem>
+              </Link>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+              <Link href="https://www.linkedin.com/in/maxime-laurendeau/" target="_blank">
+                <DropdownMenuItem className='cursor-pointer text-purple-400'>LinkedIn</DropdownMenuItem>
+              </Link>
+              <Link href="https://github.com/MaxLaur" target="_blank">
+                <DropdownMenuItem className='cursor-pointer text-purple-400'>GitHub</DropdownMenuItem>
+              </Link>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+
+        <div className="hidden md:flex text-purple-400">
+          <Link href="/">
+            <Button variant="ghost" className='m-2 mr-4 hover:bg-purple-400 hover:text-green-300 text-xl' >Home</Button>
+          </Link>
+          <Link href="https://drive.google.com/file/d/1lc-Lc300vg4dhbu7hITuV4P9kufxWEP9/view?usp=drive_link" target="_blank">
+            <Button variant="ghost" className='m-2 hover:bg-purple-400 hover:text-green-300 text-xl'>Resume</Button>
+          </Link>
+          <Link href="https://www.linkedin.com/in/maxime-laurendeau/" target="_blank">
+            <Button variant="ghost" className='m-2 hover:bg-purple-400 hover:text-green-300 text-xl'>LinkedIn</Button>
+          </Link>
+          <Link href="/projects">
+            <Button variant="ghost" className='m-2 mr-10 hover:bg-purple-400 hover:text-green-300 text-xl' >Projects</Button>
+          </Link>
+        </div>
+        {/* Effects toggle button section */}
         <div className='flex border border-double border-green-300 border-4 pl-2 pr-2'>
           <div className='flex flex-col mr-2 mt-2'>
             <p>visual</p>
             <p>effects</p>
           </div>
-          
           <div className='flex flex-col'>
             <Toggle
               defaultPressed
