5f6b8f9cd2969844b5b6b7fceec9e26e17b1abec
Max Laurendeau <maxlaurend85@gmail.com>
2024-04-12 21:49:16 -0400

    cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,32 +33,34 @@ export default function Home() {
 
   return (
     <>
-    <main onClick={closeResumeDialog} className={`flex min-h-screen flex-col items-center transition-opacity duration-1000 ${loaded ? 'opacity-100' : 'opacity-0'}`}>
-      <Navbar/>
-      <div 
-        className="absolute inset-0 bg-black opacity-10 animate-pulse-opacity"
-        style={{ backgroundImage: `url(${lazer_grid.src})`, backgroundSize: 'cover', zIndex: '-1' }}
-      />
-      <h1 className="scroll-m-20 text-3xl font-light text-green-300 md:text-5xl lg:text-7xl xl:text-8xl 2xl:text-9xl">
-        MAX LAURENDEAU
-      </h1>
-      <Card className='sm:w-5/6 md:w-5/6 lg:w-4/6 xl:w-3/6 opacity-85 mb-5 mt-5 pt-5'>
-        <CardContent>
-          <p className='text-green-300 text-3xl font-teko'>
-            Hi, my name is Max and I am a web developer, welcome to my page! Here you will find a 
-            bit of information about me and a collection of projects I have worked on. Use the terminal or the buttons above to navigate across my website.
-          </p>
-        </CardContent>
-      </Card>
-      <Terminal
-        resumeDialog={resumeDialog}
-        setResumeDialog={setResumeDialog}
-        srpDialog={srpDialog}
-        setSrpDialog={setSrpDialog}
-        aboutDialog={aboutDialog}
-        setAboutDialog={setAboutDialog}
-      />
-    </main>
+      <main onClick={closeResumeDialog} className={`flex min-h-screen flex-col items-center transition-opacity duration-1000 ${loaded ? 'opacity-100' : 'opacity-0'}`}>
+        <Navbar/>
+        <div 
+          className="absolute inset-0 bg-black opacity-10 animate-pulse-opacity"
+          style={{ backgroundImage: `url(${lazer_grid.src})`, backgroundSize: 'cover', zIndex: '-1' }}
+        />
+        <h1 className="scroll-m-20 text-3xl font-light text-green-300 md:text-5xl lg:text-7xl xl:text-8xl 2xl:text-9xl">
+          MAX LAURENDEAU
+        </h1>
+        <Card className='sm:w-5/6 md:w-5/6 lg:w-4/6 xl:w-3/6 opacity-85 mb-5 mt-5 pt-5'>
+          <CardContent>
+            <p className='text-green-300 text-3xl font-teko'>
+              Hi, my name is Max and I am a web developer. Here you will find a 
+              bit of information about me and a collection of projects I have worked on. Use the terminal or the menu above to navigate.
+            </p>
+          </CardContent>
+        </Card>
+        <Terminal
+          resumeDialog={resumeDialog}
+          setResumeDialog={setResumeDialog}
+          srpDialog={srpDialog}
+          setSrpDialog={setSrpDialog}
+          aboutDialog={aboutDialog}
+          setAboutDialog={setAboutDialog}
+          docereDialog={docereDialog}
+          setDocereDialog={setDocereDialog}
+        />
+      </main>
     </>
   );
 }
--- src/app/srp/page.tsx
+++ src/app/srp/page.tsx
@@ -1,11 +1,10 @@
 "use client"
 import Link from "next/link";
 import Image from 'next/image';
-import { Card, CardContent } from "@/components/ui/card"
+import { Card } from "@/components/ui/card"
 import Navbar from "@/components/Navbar";
 import lazer_grid from"../../../public/lazer_grid.png";
 import srpLogo from "../../../public/srp-logo-main-white.svg"
-import srpCoverImage from "../../../public/k4ntycvz.bmp"
 
 const SRP = () => {
   return (
@@ -16,28 +15,27 @@ const SRP = () => {
         style={{ backgroundImage: `url(${lazer_grid.src})`, backgroundSize: 'cover', zIndex: '-1' }}
       />
       <section>
-      <div className="flex justify-center mt-5">
-      <Image src={srpLogo} alt="SRPLogo" className="p-2"/>
-
-      </div>
-      <article className="flex flex-col items-center font-teko text-3xl mt-1 text-green-300">
-        <p>
-          This is a fullstack webpage I have made for the Shutotko Revival Project in April 2023
-        </p>
-        <div>
-          <Link
-            href='https://github.com/MaxLaur/project-srp-website'
-            target='blank'
-            className="hover:text-purple-500"
-          >Github page</Link>
+        <div className="flex justify-center mt-5">
+        <Image src={srpLogo} alt="SRPLogo" className="p-2"/>
         </div>
-        <div className="flex flex-row items-center justify-evenly">
-          <Card className="text-green-300 w-1/6 p-3">This project was made in under 2 weeks using the MERN stack with Auth0. Logged in users have the option to favorite the servers they like best</Card>
-          <iframe width="1020" height="630" src="https://www.youtube.com/embed/VKlrg3rCXeQ?si=_E0ErFOATKWE4qw8" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-          <Card className="text-green-300 w-1/6 p-3">This responsive website connects to the official SRP apis for real time server information and lap time leaderboards</Card>
-        </div>
-      </article>
-    </section>
+        <article className="flex flex-col items-center font-teko text-3xl mt-1 text-green-300">
+          <p>
+            This is a fullstack webpage I have made for the Shutotko Revival Project in April 2023
+          </p>
+          <div>
+            <Link
+              href='https://github.com/MaxLaur/project-srp-website'
+              target='blank'
+              className="hover:text-purple-500"
+            >Github page</Link>
+          </div>
+          <div className="flex flex-row items-center justify-evenly">
+            <Card className="text-green-300 w-1/6 p-3">This project was made in under 2 weeks using the MERN stack with Auth0. Logged in users have the option to favorite the servers they like best</Card>
+            <iframe width="1020" height="630" src="https://www.youtube.com/embed/VKlrg3rCXeQ?si=_E0ErFOATKWE4qw8" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+            <Card className="text-green-300 w-1/6 p-3">This responsive website connects to the official SRP apis for real time server information and lap time leaderboards</Card>
+          </div>
+        </article>
+      </section>
     </main>
   )
 }
